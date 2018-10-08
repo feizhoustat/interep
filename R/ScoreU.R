8ab0bad9b4096f03f948706480d13659edbe6c62
Fei Zhou <36722475+feizhoustat@users.noreply.github.com>
2018-10-07 21:27:08 -0500

    Update ScoreU.R

--- R/ScoreU.R
+++ R/ScoreU.R
@@ -1,7 +1,6 @@
 #' @importFrom stats gaussian
 #' @importFrom MASS ginv
 ScoreU <- function(n,k,y,x,p,response="continuous",beta,Rhat){
-  #n=as.numeric(length(y)/k)
   aindex=cumsum(k)
   index=c(0,aindex[-length(aindex)])
   U=rep(0,p)
@@ -13,16 +12,12 @@ ScoreU <- function(n,k,y,x,p,response="continuous",beta,Rhat){
     family = gaussian(link = "identity")
   }
   for (i in 1:n){
-    # D=rep(1,k) %*% t.default(x[i,])
     A=matrix(0,k[i],k[i])
     res=rep(0,k[i])
     D=x[c((1+index[i]):(k[i]+index[i])),]
     eta=D%*%beta
     mu=family$linkinv(eta)
     for (j in 1:k[i]) {
-      # D=x[j+index[i],]
-      # eta=D%*%beta
-      # mu=family$linkinv(eta)
       A[j,j]=family$variance(y)[j+index[i]]
       #res[j]=y[i,j]-mu[j]
       res[j]=y[j+index[i]]-mu[j]
