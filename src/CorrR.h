8fb758f695260583d763fb60262a4725eb0ec1e9
Fei Zhou <36722475+feizhoustat@users.noreply.github.com>
2019-10-14 11:17:51 -0500

    Delete CorrR.h

--- src/CorrR.h
+++
@@ -1,8 +0,0 @@
-#ifndef CorrR_h
-#define CorrR_h
-
-#include<RcppArmadillo.h>
-// [[Rcpp::depends(RcppArmadillo)]]
-
-arma::cube CorrR(arma::vec& y, arma::mat& x, arma::vec& beta, int n, arma::vec& k, char corre);
-#endif
